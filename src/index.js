c7b9aadbd4fe6559c97a5af0536dd5ec301a30d0
Brian B <15702512+briancodes@users.noreply.github.com>
2017-11-28 22:00:59 +0100

    Example of Promise being polyfilled (non-globally) with the babel transform runtime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import Cat from './lib/cat.js';
 import Dog from './lib/dog.js';
+
+function msgAfterTimeout(msg, who, timeout) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${msg} Hello ${who}!`), timeout);
+  });
+}
+msgAfterTimeout('', 'Foo', 100).then((msg) =>
+  msgAfterTimeout(msg, 'Bar', 200)
+).then((msg) => {
+  console.log(`done after 300ms:${msg}`);
+});
+
 export { Dog, Cat };
